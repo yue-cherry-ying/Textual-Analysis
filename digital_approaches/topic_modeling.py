2bc0e19a202a282d264b40d3be1d916f585b1d4a
yue-cherry-ying <yue.cherry.ying@outlook.com>
2021-02-23 18:04:20 -0600

    finished topic_modeling file

--- digital_approaches/topic_modeling.py
+++ digital_approaches/topic_modeling.py
@@ -25,21 +25,22 @@ def get_reuters_files(path_to_files):
 if __name__ == "__main__":
     path_to_files = sys.argv[1]
     print("Reading files...", end=" ", flush=True)
+    text_files = get_reuters_files(path_to_files)
     print("done.")
 
     print("Vectorizing texts...", end=" ", flush=True)
-    vectorizer = CountVectorizer(max_df=0.6)
+    vectorizer = CountVectorizer(max_df=0.6) # LDA works with CountVectorizer (no TF-IDF weighting)
     vectorized_data = vectorizer.fit_transform((doc[1] for doc in text_files))
     print("done.")
 
     print("Building LDA model using training set...", end=" ", flush=True)
     n_topics = int(sys.argv[2])
     lda = LatentDirichletAllocation(n_components=n_topics)
-    doc_topic_distrib = lda.fit_transform(vectorized_data)
+    doc_topic_distrib = lda.fit_transform(vectorized_data) # learns model and return document-topic matrix
     print("done.")
 
     print("\nStoring results...")
-    feature_names = vevtorizer.get_feature_names()
+    feature_names = vectorizer.get_feature_names()
     with open(f"{n_topics}.lda.topics.txt", "w") as output:
         for topic_number, topic in enumerate(lda.components_): # lda.components_ contains the topic-word matrix
             print(f"Topic #{topic_number}:", end=" ", file=output) # end=" " makes the output on the same line, not to the new line
@@ -51,9 +52,9 @@ with open(f"{n_topics}.lda.topic_distribution.txt", "w") as distribution:
     for doc, topics in enumerate(doc_topic_distrib):
         doc_name = text_files[doc][0]
         topic_values = []
-        top_topic_weight_indices = numpy.argsort(topics)[::-1][:5]
+        top_topic_weight_indices = numpy.argsort(topics)[::-1][:5] # sort topics in doc by weight and keep top 5
         for topic in top_topic_weight_indices:
             topic_weight = topics[topic]
             topic_value = f"{topic} ({topic_weight})"
             topic_values.append(topic_value)
-        print(doc_name, ", ".join())
+        print(doc_name, ", ".join(topic_values), file=distribution)
